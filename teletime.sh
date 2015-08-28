785279efd2456e8bbefef388601eeb364fe9b564
Ilya Prokin <isprokin@gmail.com>
2015-08-28 18:53:39 +0200

    correction

--- teletime.sh
+++ teletime.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-echo $1
 if [ "$1" == "" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$2" == "" ]; then
   printf " -------\nTELETIME or SSTRPIPE: a very Simple Script To Run Programs In a Past Environment\n(c) Ilya Prokin, isprokin@gmail.com, https://github.com/iprokin\n -------\nThis script redefines environmental variables so your HOME replaced with its backup. The later allows you to go back in time, i.e. to run programs so they see files from the past. \n\nTake a look at the source it's very simple. \n\nUsage:\n bash teletime.sh BACKUPNAME COMMAND\n\nE.g.:\n bash teletime.sh 2015-08-28-00:59 bash\n bash teletime.sh 2015-08-28-00:59 python\n"
 else
